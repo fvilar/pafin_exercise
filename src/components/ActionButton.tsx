6e33991abc9d5e97a3c60905bb6ba5d60c369f5b
Kevin Fondevila <kevin.fondevila@bitekso.com>
2023-11-14 01:12:08 +0900

    Abstraction of ActionButton  component

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { ArrowRight } from "@phosphor-icons/react";
+import React from "react";
 
 export default function ActionButton() {
   return (
-    <div>ActionButton</div>
-  )
+    <div className="transtaction_body_card_btn">
+      <div className="transtaction_body_card_btn_text">選択する</div>
+      <ArrowRight size={16} color="#FFFFFF"></ArrowRight>
+    </div>
+  );
 }
