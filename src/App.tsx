4b5798823ed0e7bb299edf545909dc59833ad4fd
Kevin Fondevila <kevin.fondevila@bitekso.com>
2023-11-14 00:35:31 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Assistant from "./pages/Assistant";
+function App() {
+  return (
+    <div className="App">
+      <Assistant />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QuickAccessAside.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function QuickAccessAside() {
+  return (
+    <div>QuickAccessAside</div>
+  )
+}
