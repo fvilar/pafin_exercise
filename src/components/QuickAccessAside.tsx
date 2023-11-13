e83364dd10661668025efa35aaaa31a9aaa2d742
Kevin Fondevila <kevin.fondevila@bitekso.com>
2023-11-14 01:12:56 +0900

    abstraction of QuickAccess menu and QuickAccessBtns Components

--- src/components/QuickAccessAside.tsx
+++ src/components/QuickAccessAside.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import QuickAccessBtn from "./QuickAccessBtn";
 export default function QuickAccessAside() {
   return (
-    <div>QuickAccessAside</div>
-  )
+    <div className="aside">
+      <div className="aside_title">クイックアクセス</div>
+      <div className="aside_body">
+        <QuickAccessBtn />
+        <div className="aside_quickaccess">
+          <div className="aside_quickaccess_title">帳簿設定</div>
+          <div className="aside_quickaccess_description">
+            計算方法
+            (総平均法、移動平均法)、会計通貨、データ確定に関する設定の変更ができます。
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/QuickAccessBtn.tsx
+++ src/components/QuickAccessBtn.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function QuickAccessBtn() {
   return (
-    <div>QuickAccessBtn</div>
-  )
+    <div className="aside_quickaccess">
+      <div className="aside_quickaccess_title">年度初めの残高入力</div>
+      <div className="aside_quickaccess_description">
+        過去の計算結果を引き継いで計算する場合はこちらをクリックしてください。
+      </div>
+    </div>
+  );
 }
