9eed1958661b6532c76bbaf08116818cface6932
Kevin Fondevila <kevin.fondevila@bitekso.com>
2023-11-14 01:08:37 +0900

    Created basic structure of the app with css

--- src/pages/Assistant.tsx
+++ src/pages/Assistant.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import {
+  UserCircleGear,
+  CaretUp,
+  CheckCircle,
+  ArrowRight,
+} from "@phosphor-icons/react";
+import ActionButton from "../components/ActionButton";
+import QuickAccessAside from "../components/QuickAccessAside";
+import StepContainer from "../components/StepContainer";
+import Step from "../components/Step";
 export default function Assistant() {
   return (
-    <div>Assistant</div>
-  )
+    <div className="container">
+      <div className="assistant_title">
+        クリプタクトで計算を始めてみましょう
+      </div>
+      <div className="mainsection">
+        <StepContainer
+          title="取引の情報を入力しましょう"
+          description="仮想通貨の損益計算には過去すべての取引履歴が必要になります。"
+          icon={
+            <CaretUp
+              className="transaction_counter_icon"
+              size={16}
+              color="#8C959D"
+            ></CaretUp>
+          }
+          steps={[<Step />]}
+        />
+      </div>
+      <QuickAccessAside />
+    </div>
+  );
 }
